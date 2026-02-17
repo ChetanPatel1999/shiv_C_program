666e0c48b008838d0b2d6c3feaf5f5c1d959f976
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-02-17 21:05:03 +0530

    16/02

---
+++ 20_identifire.c
@@ -0,0 +1,11 @@
+//identifire example
+#include<stdio.h>
+void main()
+{
+	int age=90;
+	int Age=56;
+	int Float=56;
+	
+	
+
+}
---
+++ 21_constant.c
@@ -0,0 +1,10 @@
+//example of constant 
+#include<stdio.h>
+void main()
+{
+    const int a=12;
+    const float pi=3.141;
+	printf("a = %d\n",a);
+//	a=90; //show error
+	printf("a = %d\n",a);
+}
