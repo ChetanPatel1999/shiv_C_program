65dcb637a91b07dca97871a03ae381655861f5f5
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-02-18 21:06:32 +0530

    16/02

---
+++ 22_airthmatic.c
@@ -0,0 +1,22 @@
+// example of airthmatic operator
+#include <stdio.h>
+void main()
+{
+    int a, b, c;
+    a = 12;
+    b = 7;
+    c = a + b;
+    printf("addition = %d\n", c);
+
+    c = a - b;
+    printf("subtraction = %d\n", c);
+
+    c = a * b;
+    printf("multiplication = %d\n", c);
+
+    c = a / b;
+    printf("division = %d\n", c);
+
+    c = a % b;
+    printf("reminder = %d\n", c);
+}
---
+++ 23_comparision_operator.c
@@ -0,0 +1,10 @@
+// example of comparision operator
+#include <stdio.h>
+void main()
+{
+    int a, b, c;
+    a = 12;
+    b = 89;
+    c = a != b;
+    printf("res = %d", c);
+}
